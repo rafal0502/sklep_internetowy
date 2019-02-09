333d58de50b7507f514fbf65af8a83a172515284
rafal0502 <rafalchojnacki0502@gmail.com>
2019-02-09 10:18:23 -0800

    delete view of cart

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -20,3 +20,15 @@ def cart_add(request, product_id):
     return redirect('cart:cart_detail')
 
 
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+
+
+
+
+
