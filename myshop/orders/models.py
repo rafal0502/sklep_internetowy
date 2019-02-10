60608ef7ac6d7bfe2121dc250f30070962c99523
rafal0502 <rafalchojnacki0502@gmail.com>
2019-02-10 04:31:08 -0800

    Added models for admin view

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from shop.models import Product
 
 class Order(models.Model):
-    fist_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     address = models.CharField(max_length=250)
