f565591bdde046064c77d82d33a0ba5b0f1ca441
rafal0502 <rafalchojnacki0502@gmail.com>
2019-02-11 12:18:48 -0800

    added paypal gate

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -3,6 +3,10 @@ from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from paypal.standard.forms import PayPalPaymentsForm
+from orders.models import Order
 
 
 def order_create(request):
@@ -20,11 +24,13 @@ def order_create(request):
                 # Usunięcie zawartości koszyka na zakupy
                 cart.clear()
                 # Uruchomienie zadania asynchronicznego
-                order_created.delay(order.id)
-                return render(request, 'orders/order/created.html',
-                              {'order': order})
+                order_created.delay(order.id) #Umieszczenie zamówienia w sesji
+                request.session['order_id'] = order.id #Przekierowanie do płatności
+                return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
 
